e0279e9d9729d94b9f8322aba2dff9ee3f0a49d8
tristen897 <tristen897@gmail.com>
2025-03-11 03:13:24 -0700

    player drawn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 
 
@@ -13,6 +13,13 @@ stop_game = 0
 
 def main():
     pygame.init
+
+
+    dt = 0
+    frame_rate_clock = pygame.time.Clock()
+
+    player_character = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -21,12 +28,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        pygame.Surface.fill(screen, [0,0,0])
 
-        pygame.Surface([SCREEN_WIDTH,SCREEN_HEIGHT])
-
-
+        player_character.draw(screen)
 
         pygame.display.flip()
+
+        
+
+
+        dt = frame_rate_clock.tick(60)
+        dt /= 1000
 
 
 
